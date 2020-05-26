065dab56bd8a6d594ce6411020775453817f1e2c
test <test@ws01>
2020-05-26 21:40:33 +0300

    First Commit
    Uploading All Files

---
+++ src/exa/exa.java
@@ -0,0 +1,20 @@
+package exa;
+
+public class exa {
+	public static void main(String[] args) {
+		int i = 0;
+		
+		switch(i) {
+		default:
+			System.out.println("default");
+			//break;
+		case 1:
+			System.out.println("1");
+			//break;
+		case 2:
+			System.out.println("2");
+		break;
+			
+		}
+	}
+}
